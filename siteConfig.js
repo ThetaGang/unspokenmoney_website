ae32df4a1d59693dce6a3435d367acf50156c723
Seth Wahle <osteth@gmail.com>
2021-03-18 15:13:11 -0500

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://www.unspokenmoney.com/`,
+    siteUrl: `https://www.unspokenmoney.com`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
@@ -10,10 +10,10 @@ module.exports = {
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Unspoken Money`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `Everything you wish they taught in school.`,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
     shortTitle: `Unspoken Money`,
